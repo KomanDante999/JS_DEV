1381e6a42a153a45be86bc06535442e2ac2f66a3
komandante999 <dreftware@yandex.ru>
2022-06-09 18:07:04 +0700

    operations 1

---
+++ 02/operatons.js
@@ -0,0 +1,18 @@
+//operations
+let x = 15 + 5;
+console.log(x);
+
+let y = 15 - 5;
+console.log(y);
+
+let z = 15 * 5;
+console.log(z);
+
+let q = 15 / 5;
+console.log(q);
+
+let w = 10 % 3 // остаток
+console.log(w);
+
+let compl = 8 + 10 * 5 - 8 / 4;
+console.log(compl);
