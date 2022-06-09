804af1dbc00a6ce3c106b2ffe93d1b778546e91b
komandante999 <dreftware@yandex.ru>
2022-06-09 11:50:27 +0700

    init

---
+++ 02/digits.js
@@ -0,0 +1,9 @@
+let digit = 20;
+let fract = 30.05;
+let exp = 2e5; //200000
+let expSmall = 2e-3; //0.002
+let expFract = 1.45e3; //1450
+let expFractSmall = 1.45e-3 //0.00145
+let fract1 = 45.; //45.0
+let fract2 = .45; //0.45
+let digit1 = -20; //-20
