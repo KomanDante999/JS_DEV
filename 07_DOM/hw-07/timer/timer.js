024718408ab8b445f0b0760f677935d90494e257
KomanDante999 <Dreftware@yandex.ru>
2022-06-22 21:35:45 +0700

    hw-07.1

--- 07_DOM/hw-07/timer/timer.js
+++ 07_DOM/hw-07/timer/timer.js
@@ -1,30 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+  let duration = 0;
+  let intervID;
   let timerInput = document.querySelector('.timer__input');
   let timerOutput = document.querySelector('.timer__output');
   let timerStart = document.querySelector('.timer__start');
 
-  function timeVal() {
-    time = Math.abs(Math.round(timerInput.value));
-    console.log(time);
-  }
+  function inputVal() {
+    let inputValue = Math.round(Math.abs(timerInput.value));
+    if (inputValue > 0) {
+      return inputValue - 1;
+    } else {
+      return inputValue;
+    }
+  };
 
-  function timeOut(n) {
-    timerOutput.textContent = n;
-  }
 
-  timerInput.addEventListener('input', timeVal )
-  timeVal();
+  function timeOut() {
+    let currentVal = parseInt(timerOutput.textContent);
+    if (currentVal === duration) {
+      clearInterval(intervID);
+    }
+    timerOutput.textContent =currentVal  +1;
+  };
 
-  var intervId;
+  timerInput.addEventListener('input', inputVal);
 
   timerStart.addEventListener('click', function() {
-
-    intervId = setInterval(timeOut(i), 1000);
-    for (let i = 0; i < time; i++) {
-      timeOut(i);
-    }
-  })
+    duration = inputVal();
+    console.log(duration);
+    clearInterval(intervID);
+    timerOutput.textContent = 0;
+    intervID = setInterval(timeOut, 1000);
+  });
 
 
 });
