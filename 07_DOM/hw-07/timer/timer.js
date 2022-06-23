175f78cc3f59005baf1614f2b32255b9181a254a
KomanDante999 <Dreftware@yandex.ru>
2022-06-23 21:44:58 +0700

    hw-07.1 remove v0.3

--- 07_DOM/hw-07/timer/timer.js
+++ 07_DOM/hw-07/timer/timer.js
@@ -1,36 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let duration = 0;
   let intervID;
   let timerInput = document.querySelector('.timer__input');
   let timerOutput = document.querySelector('.timer__output');
   let timerStart = document.querySelector('.timer__start');
 
   function inputVal() {
-    let inputValue = Math.round(Math.abs(timerInput.value));
-    if (inputValue > 0) {
-      return inputValue - 1;
-    } else {
-      return inputValue;
-    }
+      return timerInput.value;
   };
 
-
-  function timeOut() {
+  function timeOut(finalVal, step) {
     let currentVal = parseInt(timerOutput.textContent);
-    if (currentVal === duration) {
+    console.log('currentVal', currentVal);
+    if (currentVal >= finalVal) {
       clearInterval(intervID);
     }
-    timerOutput.textContent =currentVal  +1;
+    timerOutput.textContent = currentVal + step;
   };
 
   timerInput.addEventListener('input', inputVal);
 
   timerStart.addEventListener('click', function() {
-    duration = inputVal();
-    console.log(duration);
+    console.log(inputVal());
     clearInterval(intervID);
-    timerOutput.textContent = 0;
-    intervID = setInterval(timeOut, 1000);
+    step = 1;
+    delay = 1000;
+    if (inputVal() > 0) {
+      timerOutput.textContent = 0;
+      intervID = setInterval(timeOut, delay, inputVal() - 1, step);
+    };
+    if (inputVal() < 0) {
+      timerOutput.textContent = inputVal();
+      intervID = setInterval(timeOut, delay, -1, step);
+    };
+    if (inputVal() === 0) {
+      timerOutput.textContent = 0;
+    };
   });
 
 
