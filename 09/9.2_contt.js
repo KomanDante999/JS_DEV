e55bad776ec35ae5dd85dec54a6cd9012757f499
KomanDante999 <Dreftware@yandex.ru>
2022-07-10 21:32:49 +0700

    hw-09 9.2

---
+++ 09/9.2_contt.js
@@ -0,0 +1,15 @@
+const x = {name: 'Тимур'};
+console.log(x);
+x.name = 'кот'
+console.log(x);
+x.noname = x.name
+x.id = 1;
+console.log(x);
+// x = {}
+console.log(x);
+
+const s = 5;
+console.log(s);
+// s = 6;
+// const s = 6;
+console.log(s);
