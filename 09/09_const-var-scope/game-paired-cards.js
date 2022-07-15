789ebf982b0efb5a4648647f594d01d05130833c
komandante999 <dreftware@yandex.ru>
2022-07-15 17:58:58 +0700

    hw-09 rules field

--- 09/09_const-var-scope/game-paired-cards.js
+++ 09/09_const-var-scope/game-paired-cards.js
@@ -51,14 +51,109 @@
     return titile;
   }
 
-  // выбор размера игрового поля
-  function createSizeField() {
-    const wrap = document.createElement('div');
-    const input0 = document.createElement('input');
-    const input1 = document.createElement('input');
-    const input2 = document.createElement('input');
+  // модальное окно
+  function createModal(contentBody) {
+    const modal = document.createElement('div');
+    modal.classList.add('modal', 'fade');
+    modal.tabIndex = -1;
+    modal.id = 'js-modal-widow';
+    modal.setAttribute('data-bs-backdrop', 'static');
+    modal.setAttribute('data-bs-keyboard', 'false');
+    modal.setAttribute('aria-labelledby', 'staticBackdropLabel');
+    modal.setAttribute('aria-hidden', 'true');
 
+    const modalDialog = document.createElement('div');
+    modalDialog.classList.add('modal-dialog', 'modal-lg');
+    const modalConten = document.createElement('div');
+    modalConten.classList.add('modal-content');
+    const modalHeader = document.createElement('div');
+    modalHeader.classList.add('modal-header');
+    const modalTitle = document.createElement('h2');
+    modalTitle.classList.add('modal-title');
+    modalTitle.textContent = 'Выберите размер игрового поля';
+    const modalBtnClose = document.createElement('button');
+    modalBtnClose.classList.add("btn-close");
+    modalBtnClose.setAttribute('data-bs-dismiss', 'modal');
+    modalBtnClose.ariaLabel = "Close";
+    const modalBody = document.createElement('div');
+    modalBody.classList.add('modal-body');
+    const modalFooter = document.createElement('div');
+    modalFooter.classList.add('modal-footer');
+    const modalBtnStart = document.createElement('button');
+    modalBtnStart.classList.add("btn", "btn-primary");
+    modalBtnStart.setAttribute('data-bs-dismiss', 'modal');
+    modalBtnStart.textContent = 'НАЧАТЬ ИГРУ!';
+
+    modalHeader.append(modalTitle, modalBtnClose);
+    modalBody.append(contentBody);
+    modalFooter.append(modalBtnStart);
+    modalConten.append(modalHeader, modalBody, modalFooter);
+    modalDialog.append(modalConten);
+    modal.append(modalDialog);
+
+    return {modal, modalBtnStart};
   }
+
+  // кнопка изменения размера игрового поля
+  function createBtnSizeField() {
+    const wrap = document.createElement('div');
+    const btnSizeField = document.createElement('button');
+    btnSizeField.classList.add("btn", "btn-primary");
+    btnSizeField.setAttribute('data-bs-toggle', 'modal');
+    btnSizeField.setAttribute('data-bs-target', '#js-modal-widow');
+    btnSizeField.textContent = 'ВЫБРАТЬ РАЗМЕР ИГРОВОГО ПОЛЯ';
+    wrap.append(btnSizeField);
+    return wrap;
+  }
+
+  // радио чекбокс
+  function createRadioCheck(inlineClass, name, index) {
+    const wrapForm = document.createElement('div');
+    wrapForm.classList.add('form-check');
+    // инлайн класс для горизонтального расположение
+    if (inlineClass) {
+      wrapForm.classList.add(inlineClass);
+    }
+    const input = document.createElement('input');
+    input.classList.add('form-check-input');
+    input.type = 'radio';
+    input.id = name + index;
+    input.value = index;
+    input.name = name;
+    // событие выбора чекбокса
+    input.setAttribute('onchange','rulesCreateField(this.id, this.value)')
+    const label = document.createElement('label');
+    label.classList.add('form-check-label');
+    label.for = name + index;
+    label.textContent = index;
+
+    wrapForm.append(input, label);
+
+    return {wrapForm, input};
+  }
+
+  // панель выбора размера игрового поля
+  function createPanelSizeField(minSize = 1, maxSize = 4) {
+    const wrap = document.createElement('div');
+    const wrapWithField = document.createElement('div');
+    wrapWithField.classList.add('with', 'd-flex', 'flex-nowrap', 'ps-5');
+    const wrapHeightField = document.createElement('div');
+    wrapHeightField.classList.add('height');
+    wrapHeightField.style.width = '40px';
+    wrap.append(wrapWithField, wrapHeightField)
+    // горизонтальная строка
+    for (let i = minSize; i <= maxSize; i++) {
+      let radioChek = createRadioCheck('form-check-inline','withField', i).wrapForm;
+      wrapWithField.append(radioChek);
+    }
+    // вертикальная колонка
+    for (let i = minSize; i <= maxSize; i++) {
+      let radioChek = createRadioCheck('form-check-reverse','heightField', i).wrapForm;
+      wrapHeightField.append(radioChek);
+    }
+    return wrap;
+  }
+
 
   // счетчтк числа ходов
   function createCountSteps() {
@@ -86,7 +181,7 @@
   // создание карточки
   function createCard(objCard) {
     const card = document.createElement('button');
-    card.classList.add('col', 'btn', 'btn-outline-primary', 'js-card-close')
+    card.classList.add('col', 'btn', 'm-1', 'btn-outline-primary', 'js-card-close')
     card.textContent = '?';
     card.value = objCard.id;
     // обработчик клика по карте
@@ -174,6 +269,18 @@
     }
   }
 
+  // правила создания игрового поля
+  function rulesCreateField(id, value) {
+    if (value % 2 !== 0) {
+
+      if (id.includes('withField')) {
+
+        console.log(id, value);
+      }
+
+    }
+  }
+
   document.addEventListener('DOMContentLoaded', () => {
 
     // создаем массив
@@ -185,11 +292,16 @@
     const container = document.getElementById('game-paired-cards');
     container.classList.add('container');
 
-    const sizeField = createSizeField()
-    container.append(sizeField);
+    const PanelSizeField = createPanelSizeField(1, 10);
+
+    const modalStartGame = createModal(PanelSizeField);
+    container.append(modalStartGame.modal);
 
     const titleGame = createTitle(nameGame);
     container.append(titleGame);
+
+    const btnResizeField = createBtnSizeField();
+    container.append(btnResizeField);
 
     const panelCountSteps = createCountSteps();
     container.append(panelCountSteps.countWrap);
@@ -201,5 +313,6 @@
 
   })
 
+  window.rulesCreateField = rulesCreateField;
 
 })();
