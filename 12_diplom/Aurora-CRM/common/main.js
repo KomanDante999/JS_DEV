0b5159e64709c4ef1fbb85a139e35144aca7df18
KomanDante999 <Dreftware@yandex.ru>
2022-08-25 21:21:56 +0700

    aurora create html

--- 12_diplom/Aurora-CRM/common/main.js
+++ 12_diplom/Aurora-CRM/common/main.js
@@ -1,15 +1,24 @@
 import { exchangeDataFromServer } from '../modules/server-api.js';
+import { createHeader, createMain, createFooter } from '../modules/html-layout.js';
+
 let clientsDataFromServer = [];
 
 
 (() => {
   document.addEventListener('DOMContentLoaded', async () => {
+    // create wrap table
+    const container = document.getElementById('aurora-crm');
+    container.classList.add('aurora-crm');
+    const header = createHeader();
+    const main = createMain();
+    const footer = createFooter();
+    container.append(header.wrap, main.wrap, footer.wrap);
+
+
     // get data
-    clientsDataFromServer = await exchangeDataFromServer('getList');
-    console.log('clientsDataFromServer',clientsDataFromServer);
-    // create DOM elements
-    const container = document.getElementById('aurora-crm-container');
-    container.classList.add('container');
+    // clientsDataFromServer = await exchangeDataFromServer('getList');
+    // console.log('clientsDataFromServer',clientsDataFromServer);
+    // create table
   })
 })()
 
